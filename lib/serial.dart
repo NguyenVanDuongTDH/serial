b29f3e0fd0e1534b781b83e134ebdad93de82757
NguyenVanDuongTDH <154052803+NguyenVanDuongTDH@users.noreply.github.com>
2024-03-12 20:13:16 +0700

    up

--- lib/serial.dart
+++ lib/serial.dart
@@ -3,6 +3,9 @@
 /// More dartdocs go here.
 library;
 
-export 'src/serial.dart'
-    show SerialClient, SerialServer, SerialClientTCP;
+export 'src/serial.dart' show SerialClient, SerialServer;
+export 'src/tcp_client.dart' show SerialClientTCP;
+export 'src/tcp_server.dart' show SerialServerTCP;
+export 'src/windows_usb_serial.dart' show SerialWindowsClientUSB;
+export 'src/windows_usb_serial_slave.dart' show SerialWindowsServerUSB;
 export 'package:libserialport/libserialport.dart' show SerialPortParity;
